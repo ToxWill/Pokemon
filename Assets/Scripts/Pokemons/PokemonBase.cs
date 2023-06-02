532a52f4926a78da58880b7c4963b26d718e04d7
ToxWill <33481280+ToxWill@users.noreply.github.com>
2023-06-02 13:06:31 +0300

    Critical Hits & Type Effectiveness

--- Assets/Scripts/Pokemons/PokemonBase.cs
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -111,6 +111,7 @@ public class LearnableMove
 
 public enum PokemonType
 {
+    None,
     Normal,
     Fire,
     Water,
@@ -126,4 +127,29 @@ public enum PokemonType
     Rock,
     Ghost,
     Dragon
+}
+
+public class TypeChart
+{
+    static float[][] chart =
+    {
+        //                       NOR  FIR      WAT  ELE   GRA ICE FIG   POI
+        /*NOR*/    new float[] { 1f,   1f,     1f,   1f,   1f, 1f, 1f,   1f},
+        /*FIR*/    new float[] { 1f, 0.5f,   0.5f,   1f,   2f, 2f, 1f,   1f},
+        /*WAT*/    new float[] { 1f,   2f,   0.5f,   2f, 0.5f, 1f, 1f,   1f},
+        /*ELE*/    new float[] { 1f,   1f,     2f, 0.5f, 0.5f, 2f, 1f,   1f},
+        /*GRS*/    new float[] { 1f, 0.5f,     2f,   2f, 0.5f, 1f, 1f, 0.5f},
+        /*POI*/    new float[] { 1f,   1f,     1f,   1f,   2f, 1f, 1f,   1f},
+    };
+
+    public static float GetEffectiveness(PokemonType attackType, PokemonType defenseType)
+    {
+        if (attackType == PokemonType.None || defenseType == PokemonType.None)
+            return 1;
+
+        int row = (int)attackType - 1;
+        int col = (int)defenseType - 1;
+
+        return chart[row][col];
+    }
 }
