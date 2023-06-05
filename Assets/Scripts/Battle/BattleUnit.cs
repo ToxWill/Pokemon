735e620c8945039c57f2ceb634756260aea88b6e
ToxWill <33481280+ToxWill@users.noreply.github.com>
2023-06-05 22:10:00 +0300

    Refactoring Battle Logic

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -7,7 +7,12 @@ using DG.Tweening;
 public class BattleUnit : MonoBehaviour
 {
     [SerializeField] bool isPlayerUnit;
+    [SerializeField] BattleHud hud;
 
+    public bool IsPlayerUnit { get { return isPlayerUnit; } }
+
+    public BattleHud Hud { get {return hud;} }
+    
     public Pokemon Pokemon { get; set; }
 
     Image image;
@@ -28,6 +33,8 @@ public class BattleUnit : MonoBehaviour
             image.sprite = Pokemon.Base.BackSprite;
         else
             image.sprite = Pokemon.Base.FrontSprite;
+
+        hud.SetData(pokemon);
 
         image.color = originalColor;
         PlayEnterAnimation();
